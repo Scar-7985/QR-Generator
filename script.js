52997625fdecc47a1a1a51fc97f2038f5953cfa9
Nikhil <kabir708455@gmail.com>
2024-07-02 22:58:16 +0530

    Uploading on github

---
+++ script.js
@@ -0,0 +1,24 @@
+const box = document.querySelector(".box"),
+qrInput = box.querySelector(".form input"),
+generateBtn = box.querySelector(".form button"),
+qrImg = box.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        box.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        box.classList.remove("active");
+        preValue = "";
+    }
+});
